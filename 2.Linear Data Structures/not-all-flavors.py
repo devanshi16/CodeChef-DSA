bc5573512a92047a81e597cb5442150c9622f1ce
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-06-06 17:10:44 -0400

    initial commit

--- 2.Linear Data Structures/not-all-flavors.py
+++ 2.Linear Data Structures/not-all-flavors.py
@@ -4,7 +4,7 @@ for _ in range(t):
     n, k = int(n), int(k)
     seq = [int(x) for x in list(input().split())]
     check = []
-    output = []
+    output = [len(seq)]
     for i in range(k):
         check.append(i+1)
     for i in range(n):
@@ -12,24 +12,3 @@ for _ in range(t):
         if ((set(check).issubset(set(sub)))) :
             output.append(i)
     print(min(output))
-
-
-t=int(input())
-while t>0 :
-    n,k=map(int,input().split())
-    l=list(map(int,input().split()))
-    s=[]
-    for i in range(k+1) :
-        s.append(0)
-    an=0
-    for i in range(n) :
-        c=i-s[l[i]]
-        if c>an :
-            an=c
-        s[l[i]]=i+1
-    for i in range(1,k+1) :
-        c=n-s[i]
-        if c>an :
-            an=c
-    print(an)
-    t-=1
