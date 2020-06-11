00e57fe4a43e86d384482f7339c07d81f225c289
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-06-10 20:50:58 -0400

    initial commit

--- 3.Standard Template Library/save-konoha.py
+++ 3.Standard Template Library/save-konoha.py
@@ -1,3 +1,9 @@
+#Pain has a strength of Z and is confident that he will succeed. Naruto has N
+#soldiers under his command numbered 1 through N. Power of i-th soldier is denoted by Ai. 
+# When a soldier attacks pain, his strength gets reduced by the corresponding power of the soldier. 
+# However, every action has a reaction so the power of the soldier also gets halved i.e. Ai changes to [Ai/2]. 
+# Each soldier may attack any number of times (including 0). Pain is defeated if his strength is reduced to 0 or less.
+# Find the minimum number of times the soldiers need to attack so that the village is saved.
 t = int(input())
 for _ in range(t):
     n, z = input().split()
